36af01acec0311c25514e3e2a98672f26c8a81c9
Cristina Cañadas Gómez - Limón <rosten1805@protonmail.com>
2023-03-11 19:17:22 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        StreamReader file = File.OpenText("tecleado.txt");
+
+        string linea;
+        int contador = 0;
+
+
+        while ((linea = file.ReadLine()) != null && contador < 3)
+        {
+            Console.WriteLine(linea);
+            contador++;
+        }
+
+
+        file.Close();
+
+        Console.WriteLine("Programa finalizado.");
+        Console.ReadLine();
+    }
+}
